16c886242540be11c19adf51e549964fd098992d
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-12-26 13:05:44 +0200

    Turn with a compass: changed error handling and dict is created using zip

--- turn_with_a_compass.py
+++ turn_with_a_compass.py
@@ -1,19 +1,14 @@
 def direction(facing: str, turn: int):
-    assert type(facing) is str, "facing parameter must be of type str"
-    assert type(turn) is int, "turn parameter must be of type int"
-    assert turn <= 1080 and turn >= -1080, "turn parameter must be in range [-1080, 1080]" 
-    assert turn % 45 == 0, "turn parameter must be multiple of 45"
-    direction_degrees = {
-        "N": 0,
-        "NE": 45,
-        "E": 90,
-        "SE": 135,
-        "S": 180,
-        "SW": 225,
-        "W": 270,
-        "NW": 315
-    }
-    assert facing in direction_degrees.keys(), "facing parameter must one of the following: N, NE, E, SE, S, SW, W, NW"
-    return list(direction_degrees.keys())[(direction_degrees[facing] + turn) % 360 // 45]
+    if not isinstance(facing, str):
+        raise TypeError("facing parameter must be of type str")
+    if not isinstance(turn, int): 
+        raise TypeError("turn parameter must be of type int")
 
-print(direction("N", 1079))
+    assert -1080 <= turn <= 1080 and turn % 45 == 0, "turn parameter must be in range [-1080, 1080] and multiple of 45" 
+
+    directions = ("N", "NE", "E", "SE", "S", "SW", "W", "NW")
+    assert facing in directions, "facing parameter must one of the following: N, NE, E, SE, S, SW, W, NW"
+
+    direction_degrees = dict(zip(directions, [0, 45, 135, 180, 225, 270, 315]))
+
+    return list(directions)[(direction_degrees[facing] + turn) % 360 // 45]
